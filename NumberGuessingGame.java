51d9de49cb658c33b8fc887fa3e47889ed986a96
Samruddhi Jayram kamble <137019797+sjkamble16@users.noreply.github.com>
2023-07-22 23:22:28 +0530

    Add files via upload

---
+++ NumberGuessingGame.java
@@ -0,0 +1,59 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+import java.util.Random;
+import java.util.Scanner;
+public class NumberGuessingGame {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        int minRange = 1;
+        int maxRange = 100;
+        int maxAttempts = 10;
+        int userScore = 0;
+        boolean playAgain = true;
+
+        System.out.println("Welcome to the Number Guessing Game!");
+
+        while (playAgain) {
+            int secretNumber = random.nextInt(maxRange - minRange + 1) + minRange;
+            System.out.println("\nI have chosen a number between " + minRange + " and " + maxRange + ".");
+            System.out.println("You have " + maxAttempts + " attempts to guess the number.");
+
+            int attempts = 0;
+            while (attempts < maxAttempts) {
+                System.out.print("Enter your guess: ");
+                int userGuess = scanner.nextInt();
+                attempts++;
+
+                if (userGuess == secretNumber) {
+                    System.out.println("Congratulations! You guessed the correct number in " + attempts + " attempt(s)!");
+                    userScore++;
+                    break;
+                } else if (userGuess < secretNumber) {
+                    System.out.println("Too low. Try again.");
+                } else {
+                    System.out.println("Too high. Try again.");
+                }
+            }
+
+            if (attempts == maxAttempts) {
+                System.out.println("Sorry, you've reached the maximum number of attempts. The correct number was: " + secretNumber);
+            }
+
+            System.out.print("Do you want to play again? (yes/no): ");
+            String playAgainInput = scanner.next().toLowerCase();
+            if (playAgainInput.equals("no")) {
+                playAgain = false;
+            }
+        }
+
+        System.out.println("Thank you for playing the Number Guessing Game!");
+        System.out.println("Your total score is: " + userScore);
+    }
+}
